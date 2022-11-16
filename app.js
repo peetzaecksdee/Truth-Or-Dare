0d20aee1beff123414adfc96aa941981c0879072
peetzaecksdee <peetzaecksdee@hotmail.com>
2022-11-17 00:15:56 +0700

    A very random Project

---
+++ app.js
@@ -0,0 +1,32 @@
+const btn = document.getElementById("rand-btn");
+const tdText = document.getElementById("tod-text");
+const text = document.getElementById("text-display");
+const randomToD = new FileReader();
+
+function randomNumberGenerator(number) {
+	return Math.floor(Math.random() * number);
+}
+
+function loadJSON(path, success, error) {
+	const xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === XMLHttpRequest.DONE) {
+			if (xhr.status === 200) {
+				if (success) success(JSON.parse(xhr.responseText));
+			} else {
+				if (error) error(xhr);
+			}
+		}
+	};
+	xhr.open("GET", path, true);
+	xhr.send();
+}
+
+btn.addEventListener("click", () => {
+	loadJSON("random.json", (data) => {
+		const TOD = randomNumberGenerator(2) === 1 ? "Dare" : "Truth";
+		tdText.textContent = TOD;
+		const truthOrDareArray = data[TOD];
+		text.textContent = truthOrDareArray[randomNumberGenerator(500)];
+	});
+});
