6fbe6756859d951a81cf0bb425b201cedcd55660
peetzaecksdee <peetzaecksdee@hotmail.com>
2022-11-17 13:50:01 +0700

    Added Truth and Dare Buttons

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-const btn = document.getElementById("rand-btn");
+const randBtn = document.getElementById("rand-btn");
+const dareBtn = document.getElementById("dare-btn");
+const truthBtn = document.getElementById("truth-btn");
 const tdText = document.getElementById("tod-text");
 const text = document.getElementById("text-display");
 const randomToD = new FileReader();
+let truthOrDareArray;
 
 function randomNumberGenerator(number) {
-	return Math.floor(Math.random() * number);
+  return Math.floor(Math.random() * number);
 }
 
 function loadJSON(path, success, error) {
-	const xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function () {
-		if (xhr.readyState === XMLHttpRequest.DONE) {
-			if (xhr.status === 200) {
-				if (success) success(JSON.parse(xhr.responseText));
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        if (success) success(JSON.parse(xhr.responseText));
 			} else {
-				if (error) error(xhr);
+        if (error) error(xhr);
 			}
 		}
 	};
@@ -22,11 +25,17 @@ function loadJSON(path, success, error) {
 	xhr.send();
 }
 
-btn.addEventListener("click", () => {
-	loadJSON("random.json", (data) => {
-		const TOD = randomNumberGenerator(2) === 1 ? "Dare" : "Truth";
-		const truthOrDareArray = data[TOD];
-		tdText.textContent = TOD;
-		text.textContent = truthOrDareArray[randomNumberGenerator(truthOrDareArray.length)];
-	});
-});
+loadJSON("random.json", (data) => {
+  truthOrDareData = data;
+});  
+
+truthBtn.onclick = () => (truthOrDare("Truth"));
+dareBtn.onclick = () => (truthOrDare("Dare"));
+randBtn.onclick = () => (truthOrDare(randomNumberGenerator(2) === 1 ? "Dare" : "Truth"));
+
+function truthOrDare(TOD) {
+  truthOrDareArray = truthOrDareData[TOD];
+  tdText.textContent = TOD;
+  text.textContent = truthOrDareArray[randomNumberGenerator(truthOrDareArray.length)];
+}
+
